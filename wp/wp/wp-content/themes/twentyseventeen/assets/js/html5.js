cdf50db84afc2a66098b706941260bc2f03216af
Leon Stafford <leon.stafford@mac.com>
2019-01-04 02:06:20 +0700

    The commit message

--- wp/wp/wp-content/themes/twentyseventeen/assets/js/html5.js
+++ wp/wp/wp-content/themes/twentyseventeen/assets/js/html5.js
@@ -91,10 +91,10 @@
 					
 
 <form role="search" method="get" class="search-form" action="http://google.com/">
-	<label for="search-form-5c2e5a37e747b">
+	<label for="search-form-5c2e5cd02a976">
 		<span class="screen-reader-text">Search for:</span>
 	</label>
-	<input type="search" id="search-form-5c2e5a37e747b" class="search-field" placeholder="Search &hellip;" value="" name="s" />
+	<input type="search" id="search-form-5c2e5cd02a976" class="search-field" placeholder="Search &hellip;" value="" name="s" />
 	<button type="submit" class="search-submit"><svg class="icon icon-search" aria-hidden="true" role="img"> <use href="#icon-search" xlink:href="#icon-search"></use> </svg><span class="screen-reader-text">Search</span></button>
 </form>
 

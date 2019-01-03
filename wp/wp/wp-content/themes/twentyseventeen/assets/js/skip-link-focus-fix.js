78860f5cc3847e0bad48847656a76d8478729de4
Leon Stafford <leon.stafford@mac.com>
2019-01-04 02:06:26 +0700

    The commit message

--- wp/wp/wp-content/themes/twentyseventeen/assets/js/skip-link-focus-fix.js
+++ wp/wp/wp-content/themes/twentyseventeen/assets/js/skip-link-focus-fix.js
@@ -91,10 +91,10 @@
 					
 
 <form role="search" method="get" class="search-form" action="http://google.com/">
-	<label for="search-form-5c2e5a373bbe3">
+	<label for="search-form-5c2e5ccf6e0d0">
 		<span class="screen-reader-text">Search for:</span>
 	</label>
-	<input type="search" id="search-form-5c2e5a373bbe3" class="search-field" placeholder="Search &hellip;" value="" name="s" />
+	<input type="search" id="search-form-5c2e5ccf6e0d0" class="search-field" placeholder="Search &hellip;" value="" name="s" />
 	<button type="submit" class="search-submit"><svg class="icon icon-search" aria-hidden="true" role="img"> <use href="#icon-search" xlink:href="#icon-search"></use> </svg><span class="screen-reader-text">Search</span></button>
 </form>
 

b81d0a834e84fe132e93ec5092221da623824e4d
Leon Stafford <leon.stafford@mac.com>
2019-01-04 01:55:09 +0700

    The commit message

--- wp/wp/wp-content/themes/twentyseventeen/assets/js/customize-preview.js
+++ wp/wp/wp-content/themes/twentyseventeen/assets/js/customize-preview.js
@@ -91,10 +91,10 @@
 					
 
 <form role="search" method="get" class="search-form" action="http://google.com/">
-	<label for="search-form-5c2e58b9d73d9">
+	<label for="search-form-5c2e5a38a7393">
 		<span class="screen-reader-text">Search for:</span>
 	</label>
-	<input type="search" id="search-form-5c2e58b9d73d9" class="search-field" placeholder="Search &hellip;" value="" name="s" />
+	<input type="search" id="search-form-5c2e5a38a7393" class="search-field" placeholder="Search &hellip;" value="" name="s" />
 	<button type="submit" class="search-submit"><svg class="icon icon-search" aria-hidden="true" role="img"> <use href="#icon-search" xlink:href="#icon-search"></use> </svg><span class="screen-reader-text">Search</span></button>
 </form>
 

b0454a0979610411a9dbd2b45f1f1fcc4de67314
Leon Stafford <leon.stafford@mac.com>
2019-01-04 01:49:12 +0700

    The commit message

--- wp/wp/wp-content/themes/twentyseventeen/assets/js/global.js
+++ wp/wp/wp-content/themes/twentyseventeen/assets/js/global.js
@@ -91,10 +91,10 @@
 					
 
 <form role="search" method="get" class="search-form" action="http://google.com/">
-	<label for="search-form-5c2de9530c992">
+	<label for="search-form-5c2e58b63c0ac">
 		<span class="screen-reader-text">Search for:</span>
 	</label>
-	<input type="search" id="search-form-5c2de9530c992" class="search-field" placeholder="Search &hellip;" value="" name="s" />
+	<input type="search" id="search-form-5c2e58b63c0ac" class="search-field" placeholder="Search &hellip;" value="" name="s" />
 	<button type="submit" class="search-submit"><svg class="icon icon-search" aria-hidden="true" role="img"> <use href="#icon-search" xlink:href="#icon-search"></use> </svg><span class="screen-reader-text">Search</span></button>
 </form>
 
